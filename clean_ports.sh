9b0f02044f2f70b97423380dba6fab41131864fc
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2023-02-17 18:52:40 +0200

    Add files via upload

---
+++ clean_ports.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# the script performs a search and cleans forgotten work subdirectory in the FreeBSD ports tree
+
+for i in `find /usr/ports -name work -type d`
+ do
+  cd `echo "$i" | sed 's/\/[^\/]*$/\//'`
+  make clean
+ done
