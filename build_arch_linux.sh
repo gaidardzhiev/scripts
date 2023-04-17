0c2eca3c83db766462058152f6fd620d3da3437e
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2023-04-17 11:54:55 +0300

    Add files via upload

---
+++ build_arch_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#custom arch linux build
+
+export DIR=~/arch_build
+export HASH=~/openssl_user_passwd_hash
+export USER_HASH=/root/user_hash
+
+set -x
+pacman -S archiso openssl
+mkdir $DIR
+cp -r /usr/share/archiso/configs/baseline/* $DIR
+
+pacman -Q \
+	| awk '{print $1}' \
+	| sort -n > $DIR/packages.x86_64
+
+cat > $DIR/airootfs/etc/passwd << EOF
+root:x:0:0:root:/root:/usr/bin/zsh
+user:x:1000:1000::/home/user:/usr/bin/zsh
+EOF
+
+openssl passwd -6 \
+	| tee >(tail -n 1 > ~/$HASH)
+
+sed -e 's|^|user:|; s|$|::::::|' $HASH > $USER_HASH
+
+cat $USER_HASH \
+	| tee -a $DIR/airootfs/etc/shadow
