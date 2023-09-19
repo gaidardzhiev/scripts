7da11af213943c80fb7147fc1d10e9330cbc573a
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2023-09-19 14:26:03 +0300

    Add files via upload

---
+++ plan9_qemu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#wrapper script for launching plan9 in qemu
+
+disk=$1 && shift
+if [ $(uname -s) = Linux ]; then
+	kvm=-enable-kvm
+fi
+flags="-cdrom $disk -vga std"
+qemu-system-x86_64 $kvm -m 2G $flags $*
