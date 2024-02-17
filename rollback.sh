4e48c141b07e25ecc843d3d6f157a8518b3aad85
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-02-17 14:38:04 +0200

    rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -2,9 +2,8 @@
 
 grep -a upgraded /var/log/pacman.log \
         | grep $1 > /tmp/lastupdates.txt
-awk '{print $4}' /tmp/lastupdates.txt \
-        > /tmp/lines1;awk '{print $5}' \
-        /tmp/lastupdates.txt \
+awk '{print $4}' /tmp/lastupdates.txt > /tmp/lines1
+awk '{print $5}' tmp/lastupdates.txt \
         | sed 's/(/-/g' > /tmp/lines2
 paste /tmp/lines1 /tmp/lines2 > /tmp/lines
 tr -d "[:blank:]" < /tmp/lines > /tmp/packages
