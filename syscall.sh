878c1d2c0359c8d9975eadbba21680cecf24e802
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-03-13 21:19:17 +0200

    syscall.sh

--- syscall.sh
+++ syscall.sh
@@ -61,6 +61,6 @@ int main(int argc, char **argv)
 EOF
 gcc test.c -o the_test
 ./the_test 'this is a test'
-if dmesg | tail -n 1 | grep 'print_kernel syscall'; then
+if dmesg | tail -n 1 | grep -q 'print_kernel syscall'; then
 echo 'success'
 fi
