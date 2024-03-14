db2ad366b91e9c0d4444e742608a1242a537ac2a
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-03-14 11:57:23 +0200

    syscall.sh

--- syscall.sh
+++ syscall.sh
@@ -10,36 +10,58 @@ export GETNUMCPUS=`grep -c '^processor' /proc/cpuinfo`
 export JOBS='-j '$GETNUMCPUS''
 export SUFFIX="-V4N"
 
-mkdir -p $DIR
-cd $DIR
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-$VER.tar.xz
-tar xf linux-$VER.tar.xz
-cd linux-$VER
-zcat /proc/config.gz > .config
-sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-V4N"/g' .config
-sed -i '381i 457	common	print_kernel		sys_print_kernel' $TABLE
-echo "SYSCALL_DEFINE1(print_kernel, char *, msg)
-{
-  char buf[256];
-  long copied = strncpy_from_user(buf, msg, sizeof(buf));
-  if (copied < 0 || copied == sizeof(buf))
-    return -EFAULT;
-  printk(KERN_INFO "print_kernel syscall called with \"%s\"\n", buf);
-  return 0;
-}" >> $DIR/linux-$VER/$SYS
-make $JOBS
-make modules_install
-cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux$SUFFIX
-sed s/linux/linux$SUFFIX/g \
-    </etc/mkinitcpio.d/linux.preset \
-    >/etc/mkinitcpio.d/linux$SUFFIX.preset
-mkinitcpio -p linux$SUFFIX
-grub-mkconfig -o /boot/grub/grub.cfg
-echo "reboot now"
-uname -r
-cd $DIR
-touch test.c
-cat > $DIR/test.c << EOF
+touch /etc/init.d/myupdate
+cat > /etc/init.d/myupdate << EOF
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          myupdate
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+case "$1" in
+        start)
+        /home/src/1v4n/scripts/syscall.sh
+        ;;
+stop|restart|reload)
+        ;;
+esac
+EOF
+
+before_reboot(){
+        mkdir -p $DIR
+        cd $DIR
+        wget https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-$VER.tar.xz
+        tar xf linux-$VER.tar.xz
+        cd linux-$VER
+        zcat /proc/config.gz > .config
+        sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-V4N"/g' .config
+        sed -i '381i 457        common  print_kernel sys_print_kernel' $TABLE
+        echo "SYSCALL_DEFINE1(print_kernel, char *, msg)
+        {
+                char buf[256];
+                long copied = strncpy_from_user(buf, msg, sizeof(buf));
+                if (copied < 0 || copied == sizeof(buf))
+                        return -EFAULT;
+                        printk(KERN_INFO "print_kernel syscall called with \"%s\"\n", buf);
+                        return 0;
+                }" >> $DIR/linux-$VER/$SYS
+                make $JOBS
+                make modules_install
+                cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux$SUFFIX
+                sed s/linux/linux$SUFFIX/g \
+                        </etc/mkinitcpio.d/linux.preset \
+                        >/etc/mkinitcpio.d/linux$SUFFIX.preset
+                                        mkinitcpio -p linux$SUFFIX
+                                        grub-mkconfig -o /boot/grub/grub.cfg
+                                }
+
+                                after_reboot(){
+                                        uname -r
+                                        cd $DIR
+                                        touch test.c
+                                        cat > $DIR/test.c << EOF
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/syscall.h>
@@ -52,7 +74,7 @@ int main(int argc, char **argv)
   if (argc <= 1) {
     printf("provide me a string to give to system call...\n");
     return -1;
-  }
+}
   char *arg = argv[1];
   printf("making system call with \"%s\".\n", arg);
   long res = syscall(SYS_print_kernel, arg);
@@ -63,7 +85,18 @@ EOF
 gcc test.c -o the_test
 ./the_test 'this is a test'
 if dmesg | tail -n 1 | grep -q 'print_kernel syscall'; then
-echo 'success'
-else 
-echo 'error'
+        echo 'success'
+else
+        echo 'error'
+fi
+}
+if [ -f /var/run/rebooting-for-updates ]; then
+        after_reboot
+        rm /var/run/rebooting-for-updates
+        update-rc.d myupdate remove
+else
+        before_reboot
+        touch /var/run/rebooting-for-updates
+        update-rc.d myupdate defaults
+        reboot
 fi
