38b638f7b0b692eecbce025f01c30fd7bfa03b2b
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-06-01 11:05:56 +0300

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#the script determines the type of CPU architecture that is running on and echo the appropriate value
+
+case `uname -m` in
+"armv7l")
+	ARCH=armv7l
+	;;
+"x86_64")
+	ARCH=x86_64
+	;;
+"mips")
+	ARCH=mips
+	;;
+"x86")
+	ARCH=x86
+	;;
+"alpha")
+	ARCH=alpha
+	;;
+"nios2")
+	ARCH=nios2
+	;;
+"hexagon")
+	ARCH=hexagon
+	;;
+"openrisc")
+	ARCH=openrisc
+	;;
+"riscv")
+	ARCH=riscv
+	;;
+"sparc")
+	ARCH=sparc
+	;;
+"microblaze")
+	ARCH=microblaze
+	;;
+*)
+	echo "unknown platform" >&2
+exit 1
+esac
+echo $ARCH
+exit 0
