96a5dd6318f492ae124d279fda5e62a9584e5485
gaidardzhiev <1v4n@protonmail.com>
2024-10-30 11:51:36 +0200

    Wed Oct 30 09:51:36 AM UTC 2024

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ferror() {
+    echo "error: $1"
+    exit 1
+}
+
+DATE=$(date)
+
+git add . || ferror "failed to add changes"
+
+git commit -m "$DATE" || ferror "failed to commit changes"
+
+git push origin main || ferror "failed to push changes"
