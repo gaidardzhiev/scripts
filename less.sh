786284addfe6dcb8829a9055e724dddf36faccc4
gaidardzhiev <1v4n@protonmail.com>
2024-11-02 14:44:22 +0200

    Sat Nov  2 12:44:22 PM UTC 2024

--- less.sh
+++ less.sh
@@ -6,41 +6,39 @@ set -eu
 SRCLANG=${SRCLANG:-}
 
 guess_language() {
-  lang=$(echo -e ${1:-} | file - | cut -d" " -f2)
-  echo $(tr [A-Z] [a-z] <<< "$lang")
+	lang=$(echo -e ${1:-} | file - | cut -d" " -f2)
+	echo $(tr [A-Z] [a-z] <<< "$lang")
 }
 
 check_language_is_known() {
-  fallback=""
-  lang=$(source-highlight --lang-list | cut -d' ' -f1 | grep "${1:-}" || true)
-  lang=${lang:-$fallback}
-  echo $lang
+	fallback=""
+	lang=$(source-highlight --lang-list | cut -d' ' -f1 | grep "${1:-}" || true)
+	lang=${lang:-$fallback}
+	echo $lang
 }
 
 for source in "$@"; do
-  case $source in
-  	*ChangeLog|*changelog)
-      source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
-  	*Makefile|*makefile)
-      source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
-  	*.tar|*.tgz|*.gz|*.bz2|*.xz)
-      lesspipe "$source" ;;
-    *)
-      if [[ "$source" != "-" && $(basename "$source") =~ \. ]]; then
-        source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source"
-      else
-        IFS= file=$([ "source" = "-" ] && cat || cat "$source")
-        lang=$(guess_language $file)
-        lang=$(check_language_is_known $lang)
-        [ -n "$SRCLANG" ] && lang="$SRCLANG"
-        if [ -n "$lang" ]; then
-          echo $file | source-highlight --failsafe -f esc --src-lang=$lang --style-file=esc.style
-        else
-          echo $file
-        fi
-      fi
-
-      ;;
-  esac
+	case $source in
+		*ChangeLog|*changelog)
+			source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
+		*Makefile|*makefile)
+			source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
+		*.tar|*.tgz|*.gz|*.bz2|*.xz)
+			lesspipe "$source" ;;
+		*)
+			if [[ "$source" != "-" && $(basename "$source") =~ \. ]]; then
+				source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source"
+			else
+				IFS= file=$([ "source" = "-" ] && cat || cat "$source")
+				lang=$(guess_language $file)
+				lang=$(check_language_is_known $lang)
+				[ -n "$SRCLANG" ] && lang="$SRCLANG"
+				if [ -n "$lang" ]; then
+					echo $file | source-highlight --failsafe -f esc --src-lang=$lang --style-file=esc.style
+				else
+					echo $file
+				fi
+			fi
+			;;
+	esac
 done
-
