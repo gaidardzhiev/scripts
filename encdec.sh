ad934650170c44383d6e18c4df753e29efb4d99f
gaidardzhiev <1v4n@protonmail.com>
2024-11-02 15:37:05 +0200

    Sat Nov  2 01:37:05 PM UTC 2024

---
+++ encdec.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+export ARG=$1
+export TEXT=$2
+export PASS=$3
+
+fusage() {
+	printf "usage: $0 <encrypt|decrypt> <text> <password>\n"
+	printf "  encrypt: encrypts the provided TEXT using the PASSWORD\n"
+	printf "  decrypt: decrypts the provided TEXT using the PASSWORD\n"
+	exit 1
+}
+
+if [ "$#" -ne 3 ]; then
+	fusage
+fi
+
+e() {
+	echo "$TEXT" | openssl enc -e -aes-256-cbc -pbkdf2 -a -pass pass:"$PASS"
+}
+
+d() {
+	echo "$TEXT" | openssl enc -d -aes-256-cbc -pbkdf2 -a -pass pass:"$PASS"
+}
+
+case "$ARG" in
+	encrypt)
+		e
+		;;
+	decrypt)
+		d
+		;;
+	*)
+		echo "error: invalid argument '$ARG'."
+		fusage
+		;;
+esac
