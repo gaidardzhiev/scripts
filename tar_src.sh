40cec4502478395aaf525534c08b5b904052d8cf
gaidardzhiev <1v4n@protonmail.com>
2024-11-02 10:54:11 +0200

    Sat Nov  2 08:54:10 AM UTC 2024

--- tar_src.sh
+++ tar_src.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+DATE=$(date +"%Y%m%d_%H%M%S")
 DIR=/home/src/1v4n/
-FILE=$DIR/src.tar.gz
+FILE=$DIR/src_$DATE.tar.gz
 
 if ! [ -f "$FILE" ]
 then
         echo $?
 else
-        rm $FILE
+        rm "$FILE"
 fi
-tar -czvf $FILE $DIR
+tar -czvf "$FILE" "$DIR"
