779e678f5b58cf4921e1ba5218cadecd5e9b4062
gaidardzhiev <1v4n@protonmail.com>
2024-11-06 18:02:32 +0200

    Wed Nov  6 04:02:32 PM UTC 2024

--- ldd.sh
+++ ldd.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-objdump -p $1 | grep NEEDED
+fusage() {
+	printf "usage: $0 <binary_file>\n"
+	exit 1
+}
+
+fcheck() {
+	if [ ! -f "$1" ]; then
+		echo "error: file '$1' does not exist or is not a regular file..."
+		exit 2
+	fi
+}
+
+fldd() {
+	objdump -p "$1" | grep NEEDED
+}
+
+if [ "$#" -ne 1 ]; then
+	fusage
+fi
+
+fcheck "$1"
+fldd "$1"
