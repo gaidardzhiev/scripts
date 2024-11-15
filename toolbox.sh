0f2b934ebb0b459915e5fea5270b277bb5b7859f
gaidardzhiev <1v4n@protonmail.com>
2024-11-15 12:22:48 +0200

    Fri Nov 15 10:22:48 AM UTC 2024

--- toolbox.sh
+++ toolbox.sh
@@ -14,7 +14,7 @@ else
 fi
 
 fusage() {
-	echo "usage: $0 <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls> <options>"
+	echo "usage: $0 <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda> <options>"
 	exit 1
 }
 
@@ -123,6 +123,9 @@ case $CMD in
 		;;
 	ls)
 		$DIR/ls "$@"
+		;;
+	xoda)
+		$DIR/xoda "$@"
 		;;
 	*)
 		echo "unsupported command: '$CMD'"
