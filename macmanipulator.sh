8fc873ac4eb9b53d449cdf655ae1287f993e7cf1
gaidardzhiev <1v4n@protonmail.com>
2024-11-20 18:45:18 +0200

    Wed Nov 20 04:45:18 PM UTC 2024

---
+++ macmanipulator.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+DEVICE="enp0s25"
+
+rand_byte_hex() {
+	openssl rand -hex 1 2> /dev/null || head -c1 /dev/urandom | xxd -p
+}
+
+rand_mac_address() {
+	echo "$(rand_byte_hex):$(rand_byte_hex):$(rand_byte_hex):$(rand_byte_hex):$(rand_byte_hex):$(rand_byte_hex)"
+}
+
+mac_addr_show() {
+	ip addr show | grep -A 1 "${DEVICE}" | head --lines 2 | tail --lines 1 | sed 's/^ \+//' | cut -f2 -d ' '
+}
+
+change_mac_address() {
+	local new_mac
+	new_mac=$(rand_mac_address)
+	ip link set dev $DEVICE down
+	ip link set dev $DEVICE address "$new_mac"
+	ip link set dev $DEVICE up
+	echo "MAC address changed to: $new_mac"
+}
+
+echo "old MAC address: $(mac_addr_show)"
+change_mac_address
