1bbf54552ea86ced0a03ecd838db0a278408b649
gaidardzhiev <1v4n@protonmail.com>
2024-12-05 11:21:55 +0200

    Thu Dec  5 09:21:55 AM UTC 2024

---
+++ diffhex.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+	echo "usage: $0 <file1> <file2>"
+	exit 1
+fi
+
+if [ ! -f "$1" ]; then
+	echo "error: file '$1' does not exist..."
+	exit 1
+fi
+
+if [ ! -f "$2" ]; then
+	echo "error: file '$2' does not exist...."
+	exit 1
+fi
+
+diff <(hexdump -C "$1") <(hexdump -C "$2") | awk -v file1="$1" -v file2="$2" '
+/^</ {print "\033[31m" file1 ": " $0 "\033[0m"} 
+/^>/ {print "\033[32m" file2 ": " $0 "\033[0m"}'
