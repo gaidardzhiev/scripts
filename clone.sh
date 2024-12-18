652130f181f8a5ee68feeeb5d990b1d6572b4dff
gaidardzhiev <1v4n@protonmail.com>
2024-12-18 10:47:18 +0200

    Wed Dec 18 08:47:18 AM UTC 2024

--- clone.sh
+++ clone.sh
@@ -2,23 +2,23 @@
 
 set -xe
 
-GIT="git clone https://github.com/gaidardzhiev/"
+CLONE="git clone https://github.com/gaidardzhiev"
 
 if ls -l $1; then
-	printf "1\n"
+	printf "\n"
 else
 	mkdir -p $1
 	cd $1
-	git clone https://github.com/gaidardzhiev/shellcode
-	git clone https://github.com/gaidardzhiev/scripts
-	git clone https://github.com/gaidardzhiev/x86_kernel
-	git clone https://github.com/gaidardzhiev/networking
-	git clone https://github.com/gaidardzhiev/rw_file
-	git clone https://github.com/gaidardzhiev/boot_sect
-	git clone https://github.com/gaidardzhiev/on_the_metal
-	git clone https://github.com/gaidardzhiev/interceptor
-	git clone https://github.com/gaidardzhiev/libreverse
-	git clone https://github.com/gaidardzhiev/esolangs
-	git clone https://github.com/gaidardzhiev/toolbox
-	printf "0\n"
+	$CLONE/shellcode
+	$CLONE/scripts
+	$CLONE/x86_kernel
+	$CLONE/networking
+	$CLONE/rw_file
+	$CLONE/boot_sect
+	$CLONE/on_the_metal
+	$CLONE/interceptor
+	$CLONE/libreverse
+	$CLONE/esolangs
+	$CLONE/toolbox
+	printf "\n"
 fi
