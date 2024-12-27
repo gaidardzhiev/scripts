df4fa9242f1a279affabe6805508aabb42e18e46
gaidardzhiev <1v4n@protonmail.com>
2024-12-27 20:34:59 +0200

    Fri Dec 27 06:34:58 PM UTC 2024

--- spm.sh
+++ spm.sh
@@ -11,9 +11,10 @@ GCC=12.2.0
 BINUTILS=2.40
 MAKE=4.4
 STRONGSWAN=5.9.14
+MC=4.7.5.6
 
 fusage() {
-	printf "usage: $0 <tcc|gcc|make|musl|glibc|git|strongswan|zsh|bash|dash|ash|kernel|awk|grep|sed|toolbox|busybox|toybox|curl|wget|tmux|qemu|i3wm|dmenu|grub2|coreboot|flashrom>\n"
+	printf "usage: $0 <tcc|gcc|make|musl|glibc|mc|git|strongswan|zsh|bash|dash|ash|kernel|awk|grep|sed|toolbox|busybox|toybox|curl|wget|tmux|qemu|i3wm|dmenu|grub2|coreboot|flashrom>\n"
 	exit 1
 }
 
@@ -72,6 +73,24 @@ case $PKG in
 		make $JOBS
 		cp strongswan $BIN/strongswan-$STRONGSWAN-$TARGET
 		;;
+	mc)
+		cd $DIR
+		wget http://ftp.midnight-commander.org/mc-$MC.tar.xz
+		tar xf mc-$MC.tar.xz
+		rm mc-$MC.tar.xz
+		cd mc-$MC
+		./configure \
+			--without-x \
+			--disable-shared \
+			--enable-static
+		CC='gcc -static -static-libgcc -fno-exceptions'\
+		CXX='g++ -static -static-libgcc -fno-exceptions' \
+		LDFLAGS='-Wl,-static -static -lc' \
+		LIBS='-lc' \
+		make $JOBS
+		cp src/mc $BIN/mc-$MC-$TARGET
+		;;
+
 	*)
 		printf "unsupported package: '$PKG'\n"
 		fusage
