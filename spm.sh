7bd9a4c1a398db007ed9687279e94bd1a95d740e
gaidardzhiev <1v4n@protonmail.com>
2025-01-02 15:22:26 +0200

    Thu Jan  2 01:22:26 PM UTC 2025

--- spm.sh
+++ spm.sh
@@ -28,7 +28,7 @@ GIT=2.9.5
 GREP=3.11
 
 fusage() {
-	printf "usage: $0 <build-src|get-bin|delete-src|delete-bin> <tcc|gcc|make|musl|glibc|mc|git|strongswan|dietlibc|zsh|bash|dash|ash|kernel|awk|grep|sed|toolbox|busybox|toybox|qbe|curl|wget|tmux|qemu|i3wm|dmenu|grub2|coreboot|flashrom|cross|uclibc|john|nmap|lambda-delta|tmg|subc|cc500|scc|c>\n"
+	printf "usage: $0 <build-src|get-bin|delete-src|delete-bin> <tcc|gcc|make|musl|glibc|mc|git|strongswan|dietlibc|zsh|bash|dash|ash|kernel|awk|grep|sed|toolbox|busybox|toybox|qbe|curl|wget|tmux|qemu|i3wm|dmenu|grub2|coreboot|flashrom|cross|uclibc|john|nmap|lambda-delta|tmg|subc|cc500|scc|c|cproc>\n"
 	exit 1
 }
 
@@ -322,6 +322,12 @@ fbuild_src(){
 			[ "$TARGET" = x86_64 ] && {
 				cd $SRC && git clone https://github.com/andrewchambers/c && cd c && make $JOBS && make test && make selfhost
 			} || printf "unsupported CPU architecture...\n"
+			;;
+		cproc)
+			cd $SRC
+			git clone https://github.com/michaelforney/cproc
+			cd cproc
+			./configure && make $JOBS
 			;;
 		*)
 			printf "unsupported package: '$PKG'\n"
