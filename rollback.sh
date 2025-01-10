eeb90aa4ae1c6eaa04cc15ce20df70ebd8ab5d66
gaidardzhiev <1v4n@protonmail.com>
2025-01-10 11:19:58 +0200

    Fri Jan 10 09:19:58 AM UTC 2025

--- rollback.sh
+++ rollback.sh
@@ -2,12 +2,18 @@
 
 grep -a upgraded /var/log/pacman.log \
         | grep "$1" > /tmp/lastupdates.txt
+
 awk '{print $4}' /tmp/lastupdates.txt > /tmp/lines1
+
 awk '{print $5}' tmp/lastupdates.txt \
         | sed 's/(/-/g' > /tmp/lines2
+
 paste /tmp/lines1 /tmp/lines2 > /tmp/lines
+
 tr -d "[:blank:]" < /tmp/lines > /tmp/packages
+
 cd /var/cache/pacman/pkg/ || exit
+
 for i in $(cat /tmp/packages);
 do sudo pacman --noconfirm -U "$i"*;
 done
