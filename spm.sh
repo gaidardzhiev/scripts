bde081fd9c7b2dbe10e83ab9336e4018d548ed16
gaidardzhiev <1v4n@protonmail.com>
2025-02-06 09:09:56 +0000

    Thu Feb  6 09:09:56 AM UTC 2025

--- spm.sh
+++ spm.sh
@@ -813,13 +813,13 @@ fbuild_src(){
 			make defconfig
 			make menuconfig
 			make $JOBS && \
-				file busybox && \
-				make install && \
-				cd _install && \
-				mkdir -p {bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,run} && \
-				cd dev && \
-				mknod sda b 8 0 && \
-				mknod console c 5 1
+			file busybox && \
+			make install && \
+			cd _install && \
+			mkdir -p {bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,run} && \
+			cd dev && \
+			mknod sda b 8 0 && \
+			mknod console c 5 1
 			cd ../
 			touch init
 			chmod +x init
