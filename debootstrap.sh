18c551876ea93637bed6e5fe21fafd2e2c2ff71a
gaidardzhiev <1v4n@protonmail.com>
2025-02-15 08:40:07 +0000

    Sat Feb 15 08:40:07 AM UTC 2025

---
+++ debootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#the script creates an ARM debian filesystem image on an x86_64 arch Linux host
+
+create_image() {
+	export DIR="/home/mnt"
+	export VER="sid"
+	mkdir -p "$DIR"
+	dd if=/dev/zero of=/home/arm_debian_fs.img bs=512 count=5400000
+	mkfs.ext4 /home/arm_debian_fs.img
+	mount -o loop /home/arm_debian_fs.img "$DIR"
+}
+
+install_dependencies() {
+	pacman -S --noconfirm qemu-user-static qemu-user-static-binfmt debootstrap
+}
+
+bootstrap_debian() {
+	debootstrap --foreign --arch armel "$VER" "$DIR"
+	cp /usr/bin/qemu-arm-static "$DIR/usr/bin"
+	DEBIAN_FRONTEND=noninteractive \
+	DEBCONF_NONINTERACTIVE_SEEN=true \
+	LC_ALL=C \
+	LANGUAGE=C \
+	LANG=C \
+	chroot "$DIR" /debootstrap/debootstrap --second-stage
+	DEBIAN_FRONTEND=noninteractive \
+	DEBCONF_NONINTERACTIVE_SEEN=true \
+	LC_ALL=C \
+	LANGUAGE=C \
+	LANG=C \
+	chroot "$DIR" dpkg --configure -a
+}
+
+enter_chroot() {
+	printf "welcome to the chroot...\n"
+	chroot "$DIR" bash
+}
+
+create_image && install_dependencies && bootstrap_debian && enter_chroot || printf "something's wrong in here somewhere...\n"
