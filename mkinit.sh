cb6140870c1dcd9f21a6f298cbe7101a9d2d0259
gaidardzhiev <1v4n@protonmail.com>
2025-03-01 20:01:01 +0000

    Sat Mar  1 08:01:00 PM UTC 2025

--- mkinit.sh
+++ mkinit.sh
@@ -40,6 +40,6 @@ chmod +x $DIR/init
 #ldconfig
 mknod $DIR/dev/sda b 8 0
 mknod $DIR/dev/console c 5 1
-find $DIR | cpio -H newc -o | gzip > $INIT
+find $DIR | cpio -H newc -o | gzip -9 > $INIT
 rm -rf $DIR
 printf "\n\ninitramfs for $ARCH created successfully: $INIT\n"
