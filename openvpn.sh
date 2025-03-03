37b4e6c25c377528db260c8efc26bc044ffb8cfc
gaidardzhiev <1v4n@protonmail.com>
2025-03-03 08:47:11 +0000

    Mon Mar  3 08:47:10 AM UTC 2025

--- openvpn.sh
+++ openvpn.sh
@@ -8,11 +8,10 @@ printf "enter the password to access credentials:\n"
 read -s DEC
 
 [ -f "$CRED" ] && {
-	USERNAME=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | head -n 1)
-	PASSWORD=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | tail -n 1)
+	USERNAME=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | head -n 1) || exit -1
+	PASSWORD=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | tail -n 1) || exit -2
 } || {
-	printf "credentials file not found...\n"
-	exit 1
+	printf "credentials file not found...\n" && exit 1
 }
 
 #if [ -f "$CRED" ]; then
@@ -23,5 +22,5 @@ read -s DEC
 #	exit 1
 #fi
 
-printf "starting OpenVPN with configuration: $FILE\n"
+printf "starting openvpn with configuration: $FILE\n"
 echo "$PASSWORD" | openvpn --config "$FILE" --auth-user-pass <(echo -e "$USERNAME\n$PASSWORD")
