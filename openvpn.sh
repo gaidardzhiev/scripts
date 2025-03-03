b4c344216888608d14a00f95662664db350f49f7
gaidardzhiev <1v4n@protonmail.com>
2025-03-03 08:51:24 +0000

    Mon Mar  3 08:51:24 AM UTC 2025

--- openvpn.sh
+++ openvpn.sh
@@ -14,13 +14,5 @@ read -s DEC
 	printf "credentials file not found...\n" && exit 1
 }
 
-#if [ -f "$CRED" ]; then
-#	USERNAME=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | head -n 1)
-#	PASSWORD=$(openssl enc -d -aes-256-cbc -in "$CRED" -pass pass:"$DEC" -pbkdf2 | tail -n 1)
-#else
-#	printf "credentials file not found...\n"
-#	exit 1
-#fi
-
 printf "starting openvpn with configuration: $FILE\n"
 echo "$PASSWORD" | openvpn --config "$FILE" --auth-user-pass <(echo -e "$USERNAME\n$PASSWORD")
