b2e8b528de85d3064a664b998f6cf82bf3881598
gaidardzhiev <1v4n@protonmail.com>
2025-03-16 15:28:34 +0000

    Sun Mar 16 03:28:34 PM UTC 2025

--- sanitizer.sh
+++ sanitizer.sh
@@ -3,10 +3,7 @@
 IN="$1"
 OUT="$2"
 
-if [ "$#" -ne 2 ]; then
-	echo "usage: $0 <in.c> <out.c>"
-	exit 1
-fi
+[ "$#" -ne 2 ] && echo "usage: $0 <in.c> <out.c>" && exit 1
 
 awk 'NF {sub(/^[ \t]+/, ""); sub(/[ \t]+$/, ""); gsub(/[ \t]*\(\s*/,"("); gsub(/\s*\)/,")"); gsub(/[ \t]*\*\s*/,"*"); gsub(/\s*=\s*/, "="); gsub(/\s*==\s*/, "=="); gsub(/[ \t]*{\s*/, "{"); gsub(/\s*}/,"}"); gsub(/""/,"\"\""); print}' "$IN" | awk 'NF' > "$OUT"
 
