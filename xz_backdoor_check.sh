849bd0a6df75b3bbd3b2a3165c2f413f63a2d7e3
gaidardzhiev <1v4n@protonmail.com>
2025-03-18 10:10:49 +0000

    Tue Mar 18 10:10:49 AM UTC 2025

--- xz_backdoor_check.sh
+++ xz_backdoor_check.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 #the script checks if openssh is directly linked to liblzma.so and therefore is the attack vector for CVE-2024-3094 possible
 
-if ldd "$(command -v sshd)" | grep liblzma.so; then
-        echo "the attack vector for CVE-2024-3094 IS possible"
-else
-        echo "the attack vector for CVE-2024-3094 is NOT possible"
-fi
+#if ldd "$(command -v sshd)" | grep liblzma.so; then
+#        echo "the attack vector for CVE-2024-3094 IS possible"
+#else
+#        echo "the attack vector for CVE-2024-3094 is NOT possible"
+#fi
+
+fcheck() {
+	ldd "$(command -v sshd)" | grep liblzma.so && \
+		printf "the attack vector for CVE-2024-3094 IS possible\n" || \
+		printf "the attack vector for CVE-2024-3094 is NOT possible\n"
+}
+
+fcheck && exit 0 || exit 1
