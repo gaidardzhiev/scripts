dc1864e5f1fc45d2a5d38f28ab961b6aecbd9fc9
gaidardzhiev <1v4n@protonmail.com>
2025-03-22 08:14:07 +0000

    Sat Mar 22 08:14:07 AM UTC 2025

--- limine_build.sh
+++ limine_build.sh
@@ -98,7 +98,8 @@ cd ../
 
 if cc  -g \
 	-O2 \
-	-pipe -Wall -Wextra -std=c11 \
+	-pipe \
+	-Wall -Wextra -std=c11 \
 	-ffreestanding -fno-stack-protector \
 	-fno-stack-check -fno-lto -fno-pie \
 	-fno-pic -m64 -march=x86-64 -mabi=sysv \
