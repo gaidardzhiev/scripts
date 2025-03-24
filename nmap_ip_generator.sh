2530b219356523d43d71b9a999c9257c7fd43e8d
gaidardzhiev <1v4n@protonmail.com>
2025-03-24 10:16:30 +0000

    Mon Mar 24 10:16:29 AM UTC 2025

--- nmap_ip_generator.sh
+++ nmap_ip_generator.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #the script generates 25M unique IPs to scan later
 
-nmap -iR 25200000 -sL -n \
-	| awk '{print $5}' \
-	| sort -n \
-	| uniq > tmp_IPs; \
-	head -25000000 tmp_IPs > 25M_IPs;\
-	rm tmp_IPs
+fgen() {
+	nmap -iR 25200000 -sL -n \
+		| awk '{print $5}' \
+		| sort -n \
+		| uniq > tmp_IPs; \
+		head -n 25000000 tmp_IPs > 25M_IPs;\
+		rm tmp_IPs
+}
 
+fgen && exit 0 || exit 1
