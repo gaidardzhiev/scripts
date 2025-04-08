9c243cb04e204b83febc8625c8fdddd609031562
gaidardzhiev <1v4n@protonmail.com>
2025-04-08 08:38:05 +0000

    Tue Apr  8 08:38:05 AM UTC 2025

--- diffhex.sh
+++ diffhex.sh
@@ -4,10 +4,7 @@
 
 [ ! -f "$1" ] && printf "error: file '$1' does not exist...\n" && exit 2
 
-if [ ! -f "$2" ]; then
-	echo "error: file '$2' does not exist...."
-	exit 1
-fi
+[ ! -f "$2" ] && printf "error: file '$2' does not exist....\n" && exit 3
 
 diff <(hexdump -C "$1") <(hexdump -C "$2") | awk -v file1="$1" -v file2="$2" '
 /^</ {print "\033[31m" file1 ": " $0 "\033[0m"} 
