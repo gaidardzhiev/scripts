ed72df4165b738e7942873559767747b21a3a70d
gaidardzhiev <1v4n@protonmail.com>
2025-04-14 18:31:48 +0000

    Mon Apr 14 06:31:48 PM UTC 2025

--- get_syscalls.sh
+++ get_syscalls.sh
@@ -7,6 +7,14 @@ case "$(uname)" in
 		p=SYS_ ;;
 esac
 
-command -v cpp > /dev/null || { printf "cpp preprocessor not found...\n" >&2; exit 1; }
+command -v cpp > /dev/null || {
+	printf "cpp preprocessor not found...\n" >&2;
+	exit 1;
+}
 
-cpp -include sys/syscall.h -dM </dev/null | sed -n "s/^#define $p//p" | sort -k1 || { printf "something's wrong in here somewhere...\n" >&2; exit 1; }
+cpp -include sys/syscall.h -dM </dev/null \
+       	| sed -n "s/^#define $p//p" \
+       	| sort -k1 || {
+	printf "something's wrong in here somewhere...\n" >&2;
+	exit 1;
+}
