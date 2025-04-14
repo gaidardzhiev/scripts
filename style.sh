287ba316727f68284e018cc7fb6add8938a96370
gaidardzhiev <1v4n@protonmail.com>
2025-04-14 19:20:59 +0000

    Mon Apr 14 07:20:59 PM UTC 2025

--- style.sh
+++ style.sh
@@ -2,17 +2,19 @@
 
 fdepcheck() {
 	command -v "$1" >/dev/null 2>&1 || {
-		printf >&2 "error: $1 is not installed...\n"
-		exit 2
+		printf >&2 "error: $1 is not installed...\n";
+		exit 2;
 	}
 }
 
 fastyle() {
-	astyle --style=linux -T8 *.c && \
-		printf "formatting completed successfully\n" && \
-		exit 0 || \
-		printf "astyle encountered an issue while formatting\n" && \
-		exit 1
+	astyle --style=linux -T8 *.c && {
+		printf "formatting completed successfully\n";
+		exit 0;
+	} || {
+		printf "astyle encountered an issue while formatting\n";	
+		exit 1;
+	}
 }
 
 fdepcheck astyle && fastyle
