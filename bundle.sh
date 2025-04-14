56d8ee7e1ae1a22a404d5bb9d74c6c0b6d5fef62
gaidardzhiev <1v4n@protonmail.com>
2025-04-14 17:57:03 +0000

    Mon Apr 14 05:57:03 PM UTC 2025

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#the script writes on its standard output a POSIX shell script archive of files which when executed will recreate the original files
+#the script outputs a POSIX shell script archive, when it is executed it reconstructs the original files from the archive
 
 [ "$#" -lt 1 ] && {
 	printf "usage: $0 [files] >> [archive]\n";
