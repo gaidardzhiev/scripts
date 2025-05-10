71e504cd13162733d596eeb9484593b552763fe2
gaidardzhiev <1v4n@protonmail.com>
2025-05-10 08:35:26 +0000

    Sat May 10 08:35:25 AM UTC 2025

--- brute.sh
+++ brute.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-FILE="scan.gnmap"
+export FILE="scan.gnmap"
+export USR="/usr/share/brutespray/wordlist/user.txt"
+export PASS="/usr/share/brutespray/wordlist/pass.txt"
+
 
 [ -z "$1" ] && {
 	printf "usage: $0 <target>\n";
@@ -9,18 +12,17 @@ FILE="scan.gnmap"
 
 fscan() {
 	nmap -sS -sV $1 -vv -n -oA $FILE
+	return 0
 }
 
 fspray() {
-	USR="/usr/share/brutespray/wordlist/user.txt"
-	PASS="/usr/share/brutespray/wordlist/
-pass.txt"
 	brutespray \
 		--file "$FILE" \
 		-U "$USR" \
 		-P "$PASS" \
 		--threads 5 \
 		--hosts 5
+	return 0
 }
 
 { fscan && fspray && exit 0; } || printf "something's wrong in here somewhere...\n" 
