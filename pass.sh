b79289c12825c4a3c747dc4b33efebefd7f1048f
gaidardzhiev <1v4n@protonmail.com>
2025-05-12 08:39:34 +0000

    Mon May 12 08:39:34 AM UTC 2025

--- pass.sh
+++ pass.sh
@@ -15,4 +15,6 @@ z() {
 	printf "%s\n" "$Z" && return 0 || return 4
 }
 
-{ x || y || z; } && exit 0 || exit 1
+{ x || y || z; R=$?; } && exit 0 || exit 1
+
+[ "$R" -eq 0 ] 2>/dev/null || printf "%s\n" "$R"
