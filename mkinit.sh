41357343f1c1c32d7ac8b6f9d5b6895eb9e76878
gaidardzhiev <1v4n@protonmail.com>
2025-05-14 08:31:56 +0000

    Wed May 14 08:31:56 AM UTC 2025

--- mkinit.sh
+++ mkinit.sh
@@ -6,19 +6,6 @@ DIR=$(mktemp -d)
 TARGET=$(uname -m)
 ARCH="armv7m" && [ "$TARGET" = "armv8l" ] || ARCH="$TARGET"
 
-#mkdir -p $DIR/bin
-#mkdir -p $DIR/dev
-#mkdir -p $DIR/etc
-#mkdir -p $DIR/proc
-#mkdir -p $DIR/sys
-#mkdir -p $DIR/lib
-#mkdir -p $DIR/lib64
-#mkdir -p $DIR/mnt/root
-#mkdir -p $DIR/root
-#mkdir -p $DIR/sbin
-#mkdir -p $DIR/run
-#mkdir -p $DIR/usr
-
 mkdir -p $DIR/{bin,dev,etc,proc,sys,lib,lib64,mnt/root,root,sbin,run,usr}
 
 wget https://landley.net/toybox/downloads/binaries/latest/toybox-$ARCH
