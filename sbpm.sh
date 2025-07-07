2e333036424ece10b3e3b37006bd3014a65e2e0b
gaidardzhiev <1v4n@protonmail.com>
2025-07-07 11:53:27 +0000

    Mon Jul  7 11:53:27 AM UTC 2025

--- sbpm.sh
+++ sbpm.sh
@@ -51,10 +51,10 @@ fusage() {
 	printf "usage: $0 <operation> <package>\n"
 	printf "\n"
 	printf "operations:\n"
-	printf "		<build-src|get-bin|delete-src|delete-bin|update-src>\n"
+	printf "		< build-src | get-bin | delete-src | delete-bin | update-src >\n"
 	printf "\n"
 	printf "packages:\n"
-	printf "		<tcc|gcc|make|musl|glibc|mc|git|strongswan|dietlibc|zsh|bash|dash|ash|linux-kernel|awk|grep|sed|toolbox|busybox|toybox|qbe|curl|wget|tmux|qemu|i3wm|dmenu|grub|coreboot|flashrom|cross-compiler|uclibc|john|nmap|lambda-delta|tmg|subc|cc500|scc|c|cproc|9base|airgeddon|masscan|kexec|otcc|hping|esp|aboriginal|interceptor|gnupg|go|oyacc|libosmocore|libosmo-gprs|gapk|osmocom-bb|aircrack-ng|smartmontools|gdb|kmod|gzip|rsync|xz|bc|lzip|pahole|tar|bzip2|initramfs|vim|native-compiler|mkroot|avr-toolchain|diff|tcsh|jfsutils|squashfs-tools|iptables|pcmciautils|shkd>\n"
+	printf "		< tcc | gcc | make | musl | glibc | mc | git | strongswan | dietlibc | zsh | bash | dash | ash | linux-kernel | awk | grep | sed | oldbox | busybox | toybox | qbe | curl | wget | tmux | qemu | i3wm | dmenu | grub | coreboot | flashrom | cross-compiler | uclibc | john | nmap | lambda-delta | tmg | subc | cc500 | scc | c | cproc | 9base | airgeddon | masscan | kexec | otcc | hping | esp | aboriginal | interceptor | gnupg | go | oyacc | libosmocore | libosmo-gprs | gapk | osmocom-bb | aircrack-ng | smartmontools | gdb | kmod | gzip | rsync | xz | bc | lzip | pahole | tar | bzip2 | initramfs | vim | native-compiler | mkroot | avr-toolchain | diff | tcsh | jfsutils | squashfs-tools | iptables | pcmciautils | shkd | toolbox >\n"
 	exit 1
 }
 
