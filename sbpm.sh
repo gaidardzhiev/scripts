c324836a242aa378441ee0758b38e0b7991875cc
gaidardzhiev <1v4n@protonmail.com>
2025-07-11 06:43:26 +0000

    Fri Jul 11 06:43:26 AM UTC 2025

--- sbpm.sh
+++ sbpm.sh
@@ -54,7 +54,7 @@ fusage() {
 	printf "		< build-src | get-bin | delete-src | delete-bin | update-src >\n"
 	printf "\n"
 	printf "packages:\n"
-	printf "		< tcc | gcc | make | musl | glibc | mc | git | strongswan | dietlibc | zsh | bash | dash | ash | linux-kernel | awk | grep | sed | oldbox | busybox | toybox | qbe | curl | wget | tmux | qemu | i3wm | dmenu | grub | coreboot | flashrom | cross-compiler | uclibc | john | nmap | lambda-delta | tmg | subc | cc500 | scc | c | cproc | 9base | airgeddon | masscan | kexec | otcc | hping | esp | aboriginal | interceptor | gnupg | go | oyacc | libosmocore | libosmo-gprs | gapk | osmocom-bb | aircrack-ng | smartmontools | gdb | kmod | gzip | rsync | xz | bc | lzip | pahole | tar | bzip2 | initramfs | vim | native-compiler | mkroot | avr-toolchain | diff | tcsh | jfsutils | squashfs-tools | iptables | pcmciautils | shkd | toolbox >\n"
+	printf "		< tcc | gcc | make | musl | glibc | mc | git | strongswan | dietlibc | zsh | bash | dash | ash | linux-kernel | awk | grep | sed | oldbox | busybox | toybox | qbe | curl | wget | tmux | qemu | i3wm | dmenu | grub | coreboot | flashrom | cross-compiler | uclibc | john | nmap | lambda-delta | tmg | subc | cc500 | scc | c | cproc | 9base | airgeddon | masscan | kexec | otcc | hping | esp-toolchain | aboriginal | interceptor | gnupg | go | oyacc | libosmocore | libosmo-gprs | gapk | osmocom-bb | aircrack-ng | smartmontools | gdb | kmod | gzip | rsync | xz | bc | lzip | pahole | tar | bzip2 | initramfs | vim | native-compiler | mkroot | avr-toolchain | diff | tcsh | jfsutils | squashfs-tools | iptables | pcmciautils | shkd | toolbox >\n"
 	exit 1
 }
 
@@ -1209,7 +1209,7 @@ fbin() {
 					;;
 			esac
 			;;
-		esp)
+		esp-toolchain)
 			case "$TARGET" in
 				x86_64)
 					cd /opt
