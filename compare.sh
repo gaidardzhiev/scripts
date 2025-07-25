f0e79015d032414dc4ee883561791b2cd9233723
gaidardzhiev <1v4n@protonmail.com>
2025-07-25 07:47:53 +0000

    Fri Jul 25 07:47:53 AM UTC 2025

--- compare.sh
+++ compare.sh
@@ -3,7 +3,7 @@
 #the arithmetic λόγος employed in testif() and testcase() is fundamentally identical
 #both functions evaluate the divisibility of integers by 2 and 3 iterating through a sequence of numbers from 1 to 100000 applying the modulus operator to classify each number as 'even' 'divisible by three' or 'odd'
 
-testif() {
+ftestif() {
 	start=$(date +%s%N)
 	for i in $(seq 1 100000);
 	do
@@ -22,7 +22,7 @@ testif() {
 	printf "'if'   statements execution time: $elapsed nanoseconds\n"
 }
 
-testcase() {
+ftestcase() {
 	start=$(date +%s%N)
 	for i in $(seq 1 100000);
 	do
@@ -46,16 +46,16 @@ testcase() {
 	printf "'case' statements execution time: $elapsed nanoseconds\n"
 }
 
-logos() {
+flogos() {
 	sed -n '2s/^.\(.*\)/\1/p' "$0"
 	sed -n '3s/^.\(.*\)/\1/p' "$0"
 	sed -n '4s/^.\(.*\)/\1/p' "$0"
 	printf "\n"
 }
 
-logos
-p=$(testif) && printf "$p\n"
-q=$(testcase) && printf "$q\n"
+flogos
+p=$(ftestif) && printf "$p\n"
+q=$(ftestcase) && printf "$q\n"
 
 z=$(printf "$p" | sed -n 's/[^0-9]*\([0-9]*\).*/\1/p')
 x=$(printf "$q" | sed -n 's/[^0-9]*\([0-9]*\).*/\1/p')
