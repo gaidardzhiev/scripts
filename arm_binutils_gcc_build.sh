ef7082d74648dd62dc72b180aad8615ce8fc86b9
gaidardzhiev <1v4n@protonmail.com>
2025-08-12 05:18:30 +0000

    Tue Aug 12 05:18:30 AM UTC 2025

--- arm_binutils_gcc_build.sh
+++ arm_binutils_gcc_build.sh
@@ -3,7 +3,7 @@
 
 export TARGET="arm-none-eabi"
 export PREFIX="/opt/arm_gcc_binutils"
-export PATH=$PATH:$PREFIX/bin
+export PATH="$PATH:$PREFIX/bin"
 export GETNUMCPUS=`grep -c '^processor' /proc/cpuinfo`
 export JOBS='-j '$GETNUMCPUS''
 export GCC=12.2.0
