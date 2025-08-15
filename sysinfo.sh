ab5097c22bfed1f21f57833eef184dcdb4d03072
gaidardzhiev <1v4n@protonmail.com>
2025-08-15 07:36:11 +0000

    Fri Aug 15 07:36:11 AM UTC 2025

--- sysinfo.sh
+++ sysinfo.sh
@@ -274,7 +274,7 @@ main() {
 	run get_users_info
 	run get_terminal_info
 	run print_kernel_info
-	[ -z "$errors" ] || printf "${RED}function ${GREEN}%s${RESET} ${RED}failed${RESET}\n" "$errors"
+	[ -z "$errors" ] || printf "\n\n${RED}function ${GREEN}%s${RESET} ${RED}failed${RESET}\n\n" "$errors"
 }
 
 main
