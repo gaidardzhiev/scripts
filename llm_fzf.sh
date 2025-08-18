f5582c200ade168aea5eaa658b4aad6e7965cbd8
gaidardzhiev <1v4n@protonmail.com>
2025-08-18 20:46:19 +0300

    Mon Aug 18 08:46:19 PM EEST 2025

---
+++ llm_fzf.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SERVER_DIR="/home/src/llm/llama.cpp/build/bin"
+MODEL_DIR="/home/src/llm/llama.cpp/build/bin/models"
+MODEL=$(find "$MODEL_DIR" -type f | fzf --prompt="select a model: ")
+
+[ -z "$MODEL" ] && { 
+	printf "no model selected...\n"
+	exit 1
+}
+
+"$SERVER_DIR"/llama-server -m "$MODEL" --host 127.0.0.1 --port 8080
