948630be802456061a7c7b93c36db2ad6b21e50a
gaidardzhiev <1v4n@protonmail.com>
2025-08-24 07:55:41 +0000

    Sun Aug 24 07:55:41 AM UTC 2025

--- workflow.sh
+++ workflow.sh
@@ -36,37 +36,67 @@ case "$1" in
 	start)
 		printf "enter new branch name:\n"
 		read branch
-		[ -z "$branch" ] && printf "branch name cannot be empty\n" && exit 1
-		git checkout -b "$branch" || exit 1
+		[ -z "$branch" ] && \
+			printf "branch name cannot be empty\n" && \
+			exit 1
+		git checkout -b "$branch" || \
+			exit 1
 		;;
 	add)
 		printf "enter files to add (or '.' to add all):\n"
 		read files
-		[ -z "$files" ] && printf "no files specified\n" && exit 1
-		git add $files || exit 1
+		[ -z "$files" ] && \
+			printf "no files specified\n" && \
+			exit 1
+		git add $files || \
+			exit 1
 		;;
 	commit)
 		printf "enter commit message:\n"
 		read message
-		[ -z "$message" ] && printf "commit message cannot be empty\n" && exit 1
-		git commit -m "$message" || exit 1
+		[ -z "$message" ] && \
+			printf "commit message cannot be empty\n" && \
+			exit 1
+		git commit -m "$message" || \
+			exit 1
 		;;
 	push)
 		printf "pushing to remote repository...\n"
-		git push --set-upstream origin "$(git branch --show-current)" || exit 1
+		git push --set-upstream origin "$(git branch --show-current)" || \
+			exit 1
 		;;
 	pull)
 		printf "pulling latest changes from remote...\n"
-		git pull || exit 1
+		git pull || \
+			exit 1
 		;;
 	merge)
 		printf "enter branch name to merge into current branch:\n"
 		read branch
-		[ -z "$branch" ] && printf "branch name cannot be empty\n" && exit 1
-		git merge "$branch" || exit 1
+		[ -z "$branch" ] && \
+			printf "branch name cannot be empty\n" && \
+			exit 1
+		git merge "$branch" || \
+			exit 1
 		;;
 	m2m)
-		fm2m
+		current=$(git branch --show-current)
+		[ -z "$current" ] && \
+			printf "could not detect current branch\n" && \
+			exit 1
+		[ "$current" = "main" ] && \
+			printf "you are already on main branch. Nothing to merge.\n" && \
+			exit 1
+		printf "checking out main branch...\n"
+		git checkout main || \
+			{ printf "failed to checkout main branch\n"; exit 1; }
+		printf "merging branch '%s' into main...\n" "$current"
+		git merge --no-ff "$current" || \
+			{ printf "merge failed... resolve conflicts manually...\n"; exit 1; }
+		printf "pushing main branch to remote...\n"
+		git push origin main || \
+			{ printf "push failed\n"; exit 1; }
+		printf "successfully merged branch '%s' into main and pushed.\n" "$current"
 		;;
 	*)
 		fusage
