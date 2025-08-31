e586235c3413ae179b2afa0c923bdd049fdc0ce6
gaidardzhiev <1v4n@protonmail.com>
2025-08-31 08:46:53 +0000

    Sun Aug 31 08:46:53 AM UTC 2025

--- gcc_proot.sh
+++ gcc_proot.sh
@@ -13,29 +13,29 @@ export GCC="13.2.0"
 
 fbinutils() {
         mkdir -p "$DIR"
-        mkdir -p $PREFIX
-        cd $DIR
-        wget https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS.tar.bz2
-        tar -xf binutils-$BINUTILS.tar.bz2
-        cd binutils-$BINUTILS
-        rm ../binutils-$BINUTILS.tar.bz2
+        mkdir -p "$PREFIX"
+        cd "$DIR"
+        wget https://ftp.gnu.org/gnu/binutils/binutils-"$BINUTILS".tar.bz2
+        tar -xf binutils-"$BINUTILS".tar.bz2
+        cd binutils-"$BINUTILS"
+        rm ../binutils-"$BINUTILS".tar.bz2
         ./configure \
-                --target=$TARGET \
-                --prefix=$PREFIX
-        make $JOBS all
+                --target="$TARGET" \
+                --prefix="$PREFIX"
+        make "$JOBS" all
         make install && return 0 || return 2
 }
 
 fgcc() {
-        cd $DIR
-        wget https://ftp.gnu.org/gnu/gcc/gcc-$GCC/gcc-$GCC.tar.gz
-        tar -xf gcc-$GCC.tar.gz
-        cd gcc-$GCC
-        rm ../gcc-$GCC.tar.gz
+        cd "$DIR"
+        wget https://ftp.gnu.org/gnu/gcc/gcc-"$GCC"/gcc-"$GCC".tar.gz
+        tar -xf gcc-"$GCC".tar.gz
+        cd gcc-"$GCC"
+        rm ../gcc-"$GCC".tar.gz
         ./contrib/download_prerequisites
         ./configure \
-                --target=$TARGET \
-                --prefix=$PREFIX \
+                --target="$TARGET" \
+                --prefix="$PREFIX" \
                 --without-headers \
                 --with-newlib  \
                 --with-gnu-as \
@@ -46,14 +46,15 @@ fgcc() {
                 --disable-host-shared \
                 --with-boot-ldflags=-static \
                 --with-stage1-ldflags=-static
-        make $JOBS all-gcc
+        make "$JOBS" all-gcc
         make install-gcc
-        make $JOBS all-target-libgcc CFLAGS_FOR_TARGET="-g -02"
+        make "$JOBS" all-target-libgcc CFLAGS_FOR_TARGET="-g -02"
         make install-target-libgcc && return 0 || return 3
 }
 
 { fbinutils && fgcc; RET="$?" } || { 
 	printf "something is wrong in here somewhere...\n";
-	exit 1; }
+	exit 1;
+}
 
 [ "$RET" -eq 0 ] 2>/dev/null || printf "%s\n" "$RET"
