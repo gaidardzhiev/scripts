ba92da18cb317a78b66d5f453d35a20083ef91df
gaidardzhiev <1v4n@protonmail.com>
2025-08-31 08:43:08 +0000

    Sun Aug 31 08:43:08 AM UTC 2025

--- gcc_proot.sh
+++ gcc_proot.sh
@@ -2,7 +2,7 @@
 #the script compiles binutils and gcc for armv7l in termux proot
 
 export DIR="/home/src/binutils_gcc_av7l"
-export PREFIX=/opt/binutils_gcc_armv7l
+export PREFIX="/opt/binutils_gcc_armv7l"
 export TARGET=armv7l-unknown-none
 export PATH=$PATH:$PREFIX/bin
 export GETNUMCPUS=`grep -c '^processor' /proc/cpuinfo`
