a0602240679bc7a7ba97e0ad70379015af002fa1
gaidardzhiev <1v4n@protonmail.com>
2025-09-01 09:16:13 +0000

    Mon Sep  1 09:16:13 AM UTC 2025

--- oldbox.sh
+++ oldbox.sh
@@ -14,7 +14,7 @@ fdir() {
 }
 
 fusage() {
-	printf "usage: $0 <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda|cc|id|cmp|tree|kmsg|file|magic|mem|test|clear|lsblk> <options>\n"
+	printf "usage: $0 <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda|cc|id|cmp|tree|kmsg|file|magic|mem|test|clear|lsblk|systrace> <options>\n"
 	exit 1
 }
 
@@ -157,6 +157,9 @@ case $CMD in
 		;;
 	lsblk)
 		$DIR/lsblk "$@"
+		;;
+	systrace)
+		$DIR/systrace "$@"
 		;;
 	*)
 		printf "unsupported command: %s\n" "$CMD"
