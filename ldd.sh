9288b4672538157cf6ae707dbf25e3112d38a3bc
gaidardzhiev <1v4n@protonmail.com>
2025-09-30 07:04:26 +0000

    Tue Sep 30 07:04:25 AM UTC 2025

--- ldd.sh
+++ ldd.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 fusage() {
-	printf "usage: $0 <binary_file>\n";
+	printf "usage: %s <binary_file>\n" "${1}";
 	exit 1;
 }
 
 fldd() {
-	i=$(objdump -p "$1" | grep NEEDED)
+	i=$(objdump -p "${1}" | grep NEEDED)
 	[ -z "$i" ] && \
 		printf "not a dynamic executable\n" || \
-		printf "%s\n" "$i" 
+		printf "%s\n" "${i}" 
 }
 
-[ "$#" -ne 1 ] && fusage
+[ "${#}" -ne 1 ] && fusage
 
-{ fldd "$1" && exit 0; }
+{ fldd "${1}" && exit 0; }
