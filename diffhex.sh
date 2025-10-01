69f9543723ab619ae002e1d43318448f705ceebf
gaidardzhiev <1v4n@protonmail.com>
2025-10-01 08:27:11 +0000

    Wed Oct  1 08:27:11 AM UTC 2025

--- diffhex.sh
+++ diffhex.sh
@@ -15,6 +15,6 @@
 	exit 3;
 }
 
-diff <(hexdump -C "${1}") <(hexdump -C "${2}") | awk -v file1="${1}" -v file2="$2" '
+diff <(hexdump -C "${1}") <(hexdump -C "${2}") | awk -v file1="${1}" -v file2="${2}" '
 /^</ {print "\033[31m" file1 ": " $0 "\033[0m"} 
 /^>/ {print "\033[32m" file2 ": " $0 "\033[0m"}'
