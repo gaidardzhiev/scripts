cd6b9f17e795a6c6e5844b5ed202382986759833
gaidardzhiev <1v4n@protonmail.com>
2025-10-04 08:07:49 +0000

    Sat Oct  4 08:07:49 AM UTC 2025

--- git_pull.sh
+++ git_pull.sh
@@ -2,8 +2,8 @@
 
 DIR=$(find /home/src -type d -name "1v4n")
 
-for i in $(find "$DIR" -maxdepth 1 -type d)
+for i in $(find "${DIR}" -maxdepth 1 -type d)
 do
-        cd $i
-        git pull
+	cd "${i}"
+	git pull
 done
