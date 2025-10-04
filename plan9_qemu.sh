af171ea426e2667f97e32c4c9f1568ee551e9880
gaidardzhiev <1v4n@protonmail.com>
2025-10-04 08:12:12 +0000

    Sat Oct  4 08:12:12 AM UTC 2025

--- plan9_qemu.sh
+++ plan9_qemu.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #wrapper script for launching plan9 in qemu
 
-disk=$1 && shift
+disk="${1}" && shift
 
-[ $(uname -s) = Linux ] && kvm=-enable-kvm
+[ "$(uname -s)" = Linux ] && kvm=-enable-kvm
 
 flags="-cdrom $disk -vga std"
 
-qemu-system-x86_64 $kvm -m 4G $flags $*
+qemu-system-x86_64 "${kvm}" -m 4G "${flags}" "${*}"
