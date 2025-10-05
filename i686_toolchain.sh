d6c69d0f982d95b65c121bca34ca915dfafc0eeb
gaidardzhiev <1v4n@protonmail.com>
2025-10-05 07:25:27 +0000

    Sun Oct  5 07:25:27 AM UTC 2025

--- i686_toolchain.sh
+++ i686_toolchain.sh
@@ -35,7 +35,7 @@ sed -i 's/START OF targmatch.h/& \
     ;;/g' ../binutils-"${BINUTILS}"/bfd/config.bfd 
 sed -i 's/case ${generic_target} in/& \
   i386-*-toyos*)			fmt=elf ;;/g' ../binutils-"${BINUTILS}"/gas/configure.tgt
-touch ../binutils-$BINUTILS/ld/emulparams/elf_i386_toyos.sh
+touch ../binutils-"${BINUTILS}"/ld/emulparams/elf_i386_toyos.sh
 cat > ../binutils-$BINUTILS/ld/emulparams/elf_i386_toyos.sh << eof
 source_sh ${srcdir}/emulparams/elf_i386.sh
 TEXT_START_ADDR=0x08000000
