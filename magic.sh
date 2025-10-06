c8b94f461abeb778e0f4c8722733a027f2ddce02
gaidardzhiev <1v4n@protonmail.com>
2025-10-06 07:22:19 +0000

    Mon Oct  6 07:22:19 AM UTC 2025

--- magic.sh
+++ magic.sh
@@ -2,24 +2,24 @@
 #read the first 16 bytes of a file in hex and ascii for magic signature inspection
 
 z() {
-	exec 3< "$1" || { echo "error opening file"; return; }
+	exec 3< "${1}" || { echo "error opening file"; return; }
 	read -n 16 -u 3 x
 	echo -n "magic signature bytes: "
 	i=0
-	while [ "$i" -lt 16 ];
+	while [ "${i}" -lt 16 ];
 	do
 		printf "%02X " "'${x:i:1}"
 		i=$((i + 1))
 	done
 	echo
 	echo -n "human readable ASCII: "
-	i=0
-	while [ $i -lt 16 ];
+	i="0"
+	while [ ${i} -lt 16 ];
 	do
 		c="${x:i:1}"
 		case "$c" in
 			[[:print:]])
-				printf "%s" "$c"
+				printf "%s" "${c}"
 				;;
 			*)
 				printf "."
@@ -31,6 +31,6 @@ z() {
 	exec 3<&-
 }
 
-[ "$#" -ne 1 ] && { printf "usage: $0 <file>\n" >&2; exit 1; }
+[ "${#}" -ne 1 ] && { printf "usage: %s <file>\n" "${0}" >&2; exit 1; }
 
-z "$1"
+z "${1}"
