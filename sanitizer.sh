2d26a0a062c29b774a3785c4f3df689dae644634
gaidardzhiev <1v4n@protonmail.com>
2025-10-08 07:27:26 +0000

    Wed Oct  8 07:27:24 AM UTC 2025

--- sanitizer.sh
+++ sanitizer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-IN="$1"
-OUT="$2"
+IN="${1}"
+OUT="${2}"
 
 [ "$#" -ne 2 ] && {
-	printf "usage: $0 <in.c> <out.c>\n";
+	printf "usage: %s <in.c> <out.c>\n" "${0}";
 	exit 1;
 }
 
-awk 'NF {sub(/^[ \t]+/, ""); sub(/[ \t]+$/, ""); gsub(/[ \t]*\(\s*/,"("); gsub(/\s*\)/,")"); gsub(/[ \t]*\*\s*/,"*"); gsub(/\s*=\s*/, "="); gsub(/\s*==\s*/, "=="); gsub(/[ \t]*{\s*/, "{"); gsub(/\s*}/,"}"); gsub(/""/,"\"\""); print}' "$IN" | awk 'NF' > "$OUT"
+awk 'NF {sub(/^[ \t]+/, ""); sub(/[ \t]+$/, ""); gsub(/[ \t]*\(\s*/,"("); gsub(/\s*\)/,")"); gsub(/[ \t]*\*\s*/,"*"); gsub(/\s*=\s*/, "="); gsub(/\s*==\s*/, "=="); gsub(/[ \t]*{\s*/, "{"); gsub(/\s*}/,"}"); gsub(/""/,"\"\""); print}' "${IN}" | awk 'NF' > "${OUT}"
