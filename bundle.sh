45a1096df01fbd30243a017423e81afc42802ad2
gaidardzhiev <1v4n@protonmail.com>
2025-10-09 07:30:00 +0000

    fix: quote variables to prevent word splitting and globbing

--- bundle.sh
+++ bundle.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #the script outputs a POSIX shell script archive of files, when executed it reconstructs the original files from the archive
 
-[ "$#" -lt 1 ] && {
-	printf "usage: $0 [files] >> [archive]\n";
-	sed -n '2s/^.\(.*\)/\1/p' "$0";
+[ "${#}" -lt 1 ] && {
+	printf "usage: %s [files] >> [archive]\n" "${0}";
+	sed -n '2s/^.\(.*\)/\1/p' "${0}";
 	exit 1;
 }
 
 printf "#!/bin/sh\n"
 
-for f in "$@";
+for f in "${@}";
 do
-	[ -e "$f" ] && {
-		printf "cat > \"$f\" << 'HACK'\n";
-		cat "$f";
+	[ -e "${f}" ] && {
+		printf "cat > \"${f}\" << 'HACK'\n";
+		cat "${f}";
 		printf "HACK\n";
-	} || printf "$f does not exist...\n"
+	} || printf "%s does not exist...\n" "${f}"
 done
