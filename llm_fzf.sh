9cf9513aaf7d360963a494ee934f8fa01eb86788
gaidardzhiev <1v4n@protonmail.com>
2025-10-09 07:15:54 +0000

    style: quote variables for consistency

--- llm_fzf.sh
+++ llm_fzf.sh
@@ -4,11 +4,11 @@ SERVER_DIR="/home/src/llm/llama.cpp/build/bin"
 
 MODELS_DIR="/home/src/llm/llama.cpp/build/bin/models"
 
-MODEL=$(find "$MODELS_DIR" -type f | fzf --prompt="select a model: ")
+MODEL=$(find "${MODELS_DIR}" -type f | fzf --prompt="select a model: ")
 
-[ -z "$MODEL" ] && { 
+[ -z "${MODEL}" ] && { 
 	printf "no model selected...\n"
 	exit 1
 }
 
-"$SERVER_DIR"/llama-server -m "$MODEL" --host 127.0.0.1 --port 8080
+"${SERVER_DIR}"/llama-server -m "${MODEL}" --host 127.0.0.1 --port 8080
