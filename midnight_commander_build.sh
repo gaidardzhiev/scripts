2bdb6cbb879c826591ab1b063f29ab0e5f430244
gaidardzhiev <1v4n@protonmail.com>
2025-10-11 06:18:48 +0000

    fix: quote variables to prevent word splitting and globbing

--- midnight_commander_build.sh
+++ midnight_commander_build.sh
@@ -3,29 +3,29 @@
 
 export MC_VER="4.7.5.6"
 export NUMCPUS=$(grep -c '^processor' /proc/cpuinfo)
-export JOBS="-j $NUMCPUS"
+export JOBS="-j ${NUMCPUS}"
 export DIR="/opt/mc"
 
 fexit() {
-	printf "error: $1\n"
+	printf "error: ${1}\n"
 	exit 1
 }
 
 fmc() {
-	mkdir -p "$DIR" || fexit "failed to create directory $DIR"
-	cd "$DIR" || fexit "failed to change directory to $DIR"
-	wget "http://ftp.midnight-commander.org/mc-$MC_VER.tar.xz" || fexit "failed to download"
-	tar xf "mc-$MC_VER.tar.xz" || fexit "failed to extract"
-	cd "mc-$MC_VER" || fexit "failed to change directory to mc-$MC_VER"
+	mkdir -p "${DIR}" || fexit "failed to create directory ${DIR}"
+	cd "${DIR}" || fexit "failed to change directory to ${DIR}"
+	wget "http://ftp.midnight-commander.org/mc-${MC_VER}.tar.xz" || fexit "failed to download"
+	tar xf "mc-${MC_VER}.tar.xz" || fexit "failed to extract"
+	cd "mc-${MC_VER}" || fexit "failed to change directory to mc-${MC_VER}"
 	./configure --without-x --disable-shared --enable-static || fexit "configuration failed"
 	CC='gcc -static -static-libgcc -fno-exceptions' \
 	CXX='g++ -static -static-libgcc -fno-exceptions' \
 	LDFLAGS='-Wl,-static -static -lc' \
 	LIBS='-lc' \
-	make $JOBS || fexit "build failed"
-	cp "$DIR/mc-$MC_VER/src/mc" /usr/bin/mc || fexit "failed to copy mc to /usr/bin"
+	make ${JOBS} || fexit "build failed"
+	cp "${DIR}/mc-${MC_VER}/src/mc" /usr/bin/mc || fexit "failed to copy mc to /usr/bin"
 	printf "build completed successfully\n"
-	rm -r "$DIR" || fexit "failed to remove directory $DIR"
+	rm -r "${DIR}" || fexit "failed to remove directory ${DIR}"
 }
 
 fmc && exit 0 || exit 1
