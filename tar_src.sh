1dbe035b0317a26dc77c52b55981815a269b2a30
gaidardzhiev <1v4n@protonmail.com>
2025-10-11 06:16:08 +0000

    fix: quote variables to prevent word splitting and globbing

--- tar_src.sh
+++ tar_src.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DATE=$(date +"%Y%m%d_%H")
-DIR="$1"
-OUT="/home/src_$DATE.tar.gz"
+DIR="${1}"
+OUT="/home/src_${DATE}.tar.gz"
 
-[ "$#" -lt 1 ] && {
-	printf "usage: $0 <dir>\n";
+[ "${#}" -lt 1 ] && {
+	printf "usage: %s <dir>\n" "${0}";
 	exit 1;                                                 }
 
-tar -czvf "$OUT" "$DIR" && exit 0 || exit 1
+tar -czvf "${OUT}" "${DIR}" && exit 0 || exit 1
