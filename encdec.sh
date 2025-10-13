7ffece5d9175aacb3fff54b7f1e6769180ff64c0
gaidardzhiev <1v4n@protonmail.com>
2025-10-13 09:17:20 +0000

    stdin is not a file bro...

--- encdec.sh
+++ encdec.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
-export ARG="$1"
-export TEXT="$2"
-export PASS="$3"
+export ARG="${1}"
+export TEXT="${2}"
+export FILE="${2}"
+export PASS="${3}"
 
 fusage() {
-	printf "usage: %s <encrypt|decrypt> <text> <password>\n" "$0"
+	printf "usage: %s <encrypt|decrypt> <text|file> <password>\n" "${0}"
 	printf "  encrypt: encrypts the provided <text> using the provided <password>\n"
-	printf "  decrypt: decrypts the provided <text> using the provided <password>\n"
+	printf "  decrypt: decrypts the provided <file> using the provided <password>\n"
 	exit 1
 }
 
-[ "$#" -ne 3 ] && fusage
+[ "${#}" -ne 3 ] && fusage
 
-e() {
-	echo "$TEXT" | openssl enc -e -aes-256-cbc -pbkdf2 -a -pass pass:"$PASS"
+fenc() {
+	echo "${TEXT}" | openssl enc -e -aes-256-cbc -pbkdf2 -a -pass pass:"${PASS}"
 }
 
-d() {
-	echo "$TEXT" | openssl enc -d -aes-256-cbc -pbkdf2 -a -pass pass:"$PASS"
+fdec() {
+	cat "${FILE}" | openssl enc -d -aes-256-cbc -pbkdf2 -a -pass pass:"${PASS}"
 }
 
-case "$ARG" in
+case "${ARG}" in
 	encrypt)
-		e
+		fenc
 		;;
 	decrypt)
-		d
+		fdec
 		;;
 	*)
-		printf "error: invalid argument %s\n" "$ARG"
+		printf "error: invalid argument %s\n" "${ARG}"
 		fusage
 		;;
 esac
