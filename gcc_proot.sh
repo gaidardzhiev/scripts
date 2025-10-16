95005f670a309e93f3b340cc47a8baeac5bdc9a9
gaidardzhiev <1v4n@protonmail.com>
2025-10-16 06:36:34 +0000

    fix: quote variables to prevent word splitting and globbing

--- gcc_proot.sh
+++ gcc_proot.sh
@@ -4,38 +4,38 @@
 export DIR="/home/src/binutils_gcc_av7l"
 export PREFIX="/opt/binutils_gcc_armv7l"
 export TARGET="armv7l-unknown-none"
-export PATH="$PATH:$PREFIX/bin"
+export PATH="${PATH}:${PREFIX}/bin"
 export GETNUMCPUS=`grep -c '^processor' /proc/cpuinfo`
-export JOBS='-j '$GETNUMCPUS''
+export JOBS='-j '${GETNUMCPUS}''
 export BINUTILS="2.42"
 export GCC="13.2.0"
 
 
 fbinutils() {
-        mkdir -p "$DIR"
-        mkdir -p "$PREFIX"
-        cd "$DIR"
-        wget https://ftp.gnu.org/gnu/binutils/binutils-"$BINUTILS".tar.bz2
-        tar -xf binutils-"$BINUTILS".tar.bz2
-        cd binutils-"$BINUTILS"
-        rm ../binutils-"$BINUTILS".tar.bz2
+        mkdir -p "${DIR}"
+        mkdir -p "${PREFIX}"
+        cd "${DIR}"
+        wget https://ftp.gnu.org/gnu/binutils/binutils-"${BINUTILS}".tar.bz2
+        tar -xf binutils-"${BINUTILS}".tar.bz2
+        cd binutils-"${BINUTILS}"
+        rm ../binutils-"${BINUTILS}".tar.bz2
         ./configure \
-                --target="$TARGET" \
-                --prefix="$PREFIX"
-        make "$JOBS" all
+                --target="${TARGET}" \
+                --prefix="${PREFIX}"
+        make "${JOBS}" all
         make install && return 0 || return 2
 }
 
 fgcc() {
-        cd "$DIR"
-        wget https://ftp.gnu.org/gnu/gcc/gcc-"$GCC"/gcc-"$GCC".tar.gz
-        tar -xf gcc-"$GCC".tar.gz
-        cd gcc-"$GCC"
-        rm ../gcc-"$GCC".tar.gz
+        cd "${DIR}"
+        wget https://ftp.gnu.org/gnu/gcc/gcc-"${GCC}"/gcc-"${GCC}".tar.gz
+        tar -xf gcc-"${GCC}".tar.gz
+        cd gcc-"${GCC}"
+        rm ../gcc-"${GCC}".tar.gz
         ./contrib/download_prerequisites
         ./configure \
-                --target="$TARGET" \
-                --prefix="$PREFIX" \
+                --target="${TARGET}" \
+                --prefix="${PREFIX}" \
                 --without-headers \
                 --with-newlib  \
                 --with-gnu-as \
@@ -46,15 +46,15 @@ fgcc() {
                 --disable-host-shared \
                 --with-boot-ldflags=-static \
                 --with-stage1-ldflags=-static
-        make "$JOBS" all-gcc
+        make "${JOBS}" all-gcc
         make install-gcc
-        make "$JOBS" all-target-libgcc CFLAGS_FOR_TARGET="-g -02"
+        make "${JOBS}" all-target-libgcc CFLAGS_FOR_TARGET="-g -02"
         make install-target-libgcc && return 0 || return 3
 }
 
-{ fbinutils && fgcc; RET="$?" } || { 
+{ fbinutils && fgcc; RET="${?}" } || { 
 	printf "something is wrong in here somewhere...\n";
 	exit 1;
 }
 
-[ "$RET" -eq 0 ] 2>/dev/null || printf "%s\n" "$RET"
+[ "${RET}" -eq 0 ] 2>/dev/null || printf "%s\n" "${RET}"
