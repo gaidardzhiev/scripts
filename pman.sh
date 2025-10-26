c0cb30ee422bfbe305c5cbb0b9f0837ff915d77a
gaidardzhiev <1v4n@protonmail.com>
2025-10-26 08:29:21 +0000

    fix: reject duplicates and avoid decrypt clash

--- pman.sh
+++ pman.sh
@@ -1,52 +1,56 @@
 #!/bin/sh
 
 DB=/root/.pdb
-KEY=$3
+KEY="${3}"
 
 fgen() {
 	tr -dc '[:graph:]' < /dev/urandom | head -c 16
 }
 
 fenc() {
-	echo "$1" | openssl enc -aes-256-cbc \
-		-a -salt -pass pass:"$KEY" \
+	echo "${1}" | openssl enc -aes-256-cbc \
+		-a -salt -pass pass:"${KEY}" \
 		-pbkdf2 -iter 100000
 }
 
 fdec() {
-	echo "$1" | openssl enc -aes-256-cbc \
-		-d -a -pass pass:"$KEY" \
+	echo "${1}" | openssl enc -aes-256-cbc \
+		-d -a -pass pass:"${KEY}" \
 		-pbkdf2 -iter 100000
 }
 
 fadd() {
+	grep -q "^${1}:" "${DB}" && {
+		printf "entry for %s already exists...\n" "${1}" >&2
+		exit 1
+	}
 	DPASS=$(fgen)
-	EPASS=$(fenc "$DPASS")
-	echo "$1:$EPASS" >> "$DB"
-	echo "password for $1 added"
+	EPASS=$(fenc "${DPASS}")
+	echo "${1}:${EPASS}" >> "${DB}"
+	echo "password for ${1} added"
 }
 
 fget() {
-	case $(grep -q "^$1:" "$DB"; echo $?) in #capture exit status
+	case $(grep -q "^${1}:" "${DB}"; echo ${?}) in
 		0)
-			EPASS=$(grep "^$1:" "$DB" | cut -d':' -f2)
-			DPASS=$(fdec "$EPASS")
-			echo "password for $1: $DPASS"
+			EPASS=$(grep "^${1}:" "${DB}" | cut -d':' -f2)
+			DPASS=$(fdec "${EPASS}")
+			echo "password for ${1}: ${DPASS}"
 			;;
 		1)
-			echo "no entry found for $1..."
+			echo "no entry found for ${1}..."
 			;;
 	esac
 }
 
-case "$1" in
+case "${1}" in
 	add)
-		fadd "$2"
+		fadd "${2}"
 		;;
 	get)
-		fget "$2"
+		fget "${2}" #"${3}"
 		;;
 	*)
-		printf "usage: %s <add|get> <name> <key>\n" "$0"
+		printf "usage: %s <add|get> <name> <key>\n" "${0}"
 		;;
 esac
